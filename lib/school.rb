fdc921ef989460de8074c8dbe87cec2e175a3648
vuonga1103 <vuonga1103@yahoo.com>
2020-06-23 17:10:12 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+require "pry"
+
+class School
+  attr_reader :name, :roster
+  def initialize(name)
+    @name = name
+    @roster = Hash.new { |h, k| h[k] = [] } # format the key-val pairs, val will be an array
+  end
+
+  def add_student(name, grade)
+    @roster[grade] << name
+  end
+
+  def grade(grade_num)
+    @roster[grade_num]
+  end
+
+  def sort
+    new_hash = {}
+    @roster.each { |grade, students| new_hash[grade] = students.sort }
+    new_hash
+  end
+end
