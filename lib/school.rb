0048916ae91adabf69e7e50ac4a378ed88446e43
“DonatelloTHM” <donat.pllana@outlook.com>
2020-06-23 18:10:48 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -9,22 +9,20 @@ class School
          
     end
     def add_student(student_name,grade)
-        @grade=grade
-        @student=student_name
-        if(!@roster[@grade])
-        @roster[@grade]=[]
+        if(!roster[grade])
+        roster[grade]=[]
         end
-        @roster[@grade].push(@student)
+        roster[grade].push(student_name)
 
         
     end
     def grade(grade)
-        @roster[grade]
+        roster[grade]
 
     end
     def sort
-        @roster.each do |key,value|
-            @roster[key]=value.sort
+        roster.each do |key,value|
+            roster[key]=value.sort
         end
     end
 end
