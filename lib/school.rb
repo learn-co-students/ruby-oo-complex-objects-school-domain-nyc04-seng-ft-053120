caa730ac5301a78ea027c81f7371eee0f893870c
kceron <karemiceron@gmail.com>
2020-06-23 15:06:49 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,13 +1,10 @@
 require 'pry'
 class School
-    
+    attr_accessor :roster
+
     def initialize(name)
         @name = name
         @roster = {}
-    end
-
-    def roster 
-        @roster
     end
 
     def add_student(student, grade)
