33febcba08a44a57e4438372fe9ae3dede270b45
sarahmr63089 <sarahrose63089@gmail.com>
2020-06-23 16:43:16 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require "pry"
+class School
+    def initialize(school_name)
+        @school_name = school_name
+        @roster = {}
+    end
+
+    attr_reader :roster
+
+    def add_student(student_name, grade)
+        if @roster.has_key?(grade)
+            #add student to array at grade
+            @roster[grade] << student_name
+        else #create new hash pair
+            @roster[grade] = []
+            @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster = @roster.each { |grade, class_list| @roster[grade] = class_list.sort}
+        @roster
+    end
+
+end
+
+# bayside = School.new("Bayside")
+
+# bayside.add_student("Zach Morris", 9)
