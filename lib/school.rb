0ea10b91c4c61a57799c78a29848f6efc4c577a9
Rouan Thompson <rouan.thompson@gmail.com>
2020-06-23 21:07:33 -0400

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School
+    attr_reader :roster, :name
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    #setter
+    def add_student(student, grade)
+        if roster.has_key?(grade)
+            @roster[grade] << student
+        else
+            @roster[grade] = [student]
+        end
+    end
+
+    #getter
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+       @roster.each do |grade, student|
+            student.sort!
+       end
+    end
+end
